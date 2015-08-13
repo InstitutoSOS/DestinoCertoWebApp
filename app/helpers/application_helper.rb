2a1db1986280a78574e99f8b81a609e7029eb338
Guilherme Pontes <guilhermepontes@msn.com>
2015-08-13 12:47:49 -0300

    Material listing helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def material_list
+    materials = t 'materials'
+    elements = []
+
+    materials.each do |key, value|
+      button = button_tag(value, data: { value: value })
+      elements << content_tag(:div, button, class: :material)
+    end
+
+    safe_join(elements)
+  end
 end
