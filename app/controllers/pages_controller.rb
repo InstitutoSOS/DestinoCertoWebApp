582d3f120e6ac0d46528f66358541106f2c7c146
Guilherme Pontes <guilhermepontes@msn.com>
2015-08-13 15:39:01 -0300

    Using only the allowed materials in results page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,14 @@ class PagesController < ApplicationController
   end
 
   def results
+    materials = t('materials')
+    material  = params[:material].to_sym
+
+    if materials.has_key?(material)
+      @material = materials[material]
+    else
+      redirect_to root_path
+    end
   end
 
   def signup
