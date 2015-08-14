11036632659510aa45bf3dc0d30376fa4a0ac0ba
Guilherme Pontes <guilhermepontes@msn.com>
2015-08-14 13:33:13 -0300

    Homepage script.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,14 +3,21 @@ DestinoCerto.HomePage= (function(){
   'use strict';
 
   var __private = {
-    cache : function(){},
-    bind  : function(){}
+    cache : function(){
+      this.items = $('.material');
+    },
+
+    animate: function(){
+      this.items.each(function(index) {
+        $(this).delay(250 * index).fadeIn(1000);
+      });
+    }
   };
 
   return {
     init: function(){
       __private.cache();
-      __private.bind();
+      __private.animate();
     }
   };
 })();
