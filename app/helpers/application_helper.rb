2446f1f65c3b888259444318d80b4ec98ff8d5ab
Guilherme Pontes <guilhermepontes@msn.com>
2015-08-14 13:34:56 -0300

    Helper to show all materials.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,25 @@ module ApplicationHelper
     elements = []
 
     materials.each do |key, value|
-      button = button_tag(value, data: { value: value })
-      elements << content_tag(:div, button, class: :material)
+      span = content_tag(:span, value)
+      link = link_to(span, "material/#{key}", class: "material-item material-#{key}")
+      elements << content_tag(:div, link, class: :material)
     end
 
     safe_join(elements)
+  end
+
+  def material_links
+    materials = t 'materials'
+    links = []
+
+    current_material = params[:material]
+
+    materials.each do |key, value|
+      is_active = (key.to_s == current_material) ? 'active' : ''
+      links << link_to(value, "/material/#{key}/", class: is_active)
+    end
+
+    safe_join(links)
   end
 end
